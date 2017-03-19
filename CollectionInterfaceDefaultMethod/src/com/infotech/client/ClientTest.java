3a542f74c4bd421eb2d274db1b5777498546583d
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-03-19 20:25:49 +0530

    CollectionInterfaceDefaultMethod Source code

---
+++ CollectionInterfaceDefaultMethod/src/com/infotech/client/ClientTest.java
@@ -0,0 +1,29 @@
+package com.infotech.client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.infotech.model.Employee;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+		
+		List<Employee> empList = new ArrayList<>();
+		
+		empList.add(new Employee("Raj", 90000.00, "HR"));
+		empList.add(new Employee("Frank", 80000.00, "Finance"));
+		empList.add(new Employee("Kishan", 30000.00, "HR"));
+		empList.add(new Employee("Sean", 90000.00, "Finance"));
+		empList.add(new Employee("Rajesh", 90000.00, "HR"));
+		
+		empList.forEach(s->System.out.println(s));
+		System.out.println("------------------------------------------------");
+		
+		empList.removeIf(s->"HR".equalsIgnoreCase(s.getDeptName()));
+		
+		empList.forEach(s->System.out.println(s));
+		
+	}
+
+}
